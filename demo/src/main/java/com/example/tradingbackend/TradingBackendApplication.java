45f70209bb8b61dc280a2f58b5c9207a4a1abf9b
CCW1210 <sammyhp2004@gmail.com>
2025-02-24 20:15:10 +0800

    add env setting

--- demo/src/main/java/com/example/tradingbackend/TradingBackendApplication.java
+++ demo/src/main/java/com/example/tradingbackend/TradingBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.tradingbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TradingBackendApplication {
 
 	public static void main(String[] args) {
+		// 載入 .env 文件，並將每個條目設定為系統屬性
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing() // 如果 .env 文件不存在則忽略
+				.load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(TradingBackendApplication.class, args);
 	}
-
 }
